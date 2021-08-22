eeba9f9d78147e181a825f351ad73b8d57392eac
kkk <521luqiya@gmail.com>
2021-08-22 10:36:31 +0800

    add init && start

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  title: 'jq 源码学习笔记',
+  description: 'Just playing around',
+  head: [
+    ['meta', { name: 'referrer', content: 'no-referrer' }],
+  ],
+  themeConfig: {
+    sidebar: [
+      ['/01 开始.md', '开始'],
+
+    ],
+    displayAllHeaders: true,
+    smoothScroll: true,
+  }
+}
